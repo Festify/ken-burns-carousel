af3099f7c761b15f70c0cd5bfc57393919120de4
Leo Bernard <admin@leolabs.org>
2018-03-15 20:19:18 +0100

    :sparkles: Add demo page with 5 examples

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import minify from 'rollup-plugin-babel-minify';
+import cjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import replace from 'rollup-plugin-replace';
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+    input: 'src/ken-burns-carousel.ts',
+    output: {
+        file: 'dist/ken-burns-carousel.min.js',
+        format: 'umd',
+        name: 'KenBurnsSlideshow',
+        sourcemap: true,
+    },
+    plugins: [
+        nodeResolve({ browser: true }),
+        typescript(),
+        cjs(),
+        replace({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'develop'),
+        }),
+        minify({ comments: false }),
+    ],
+};
